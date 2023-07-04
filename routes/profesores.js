321288331bf2dbac4d168f1eb8004160895f2e9a
r0drigoHerrera <rodherrera14@gmail.com>
2023-07-04 20:41:35 -0300

    validación post profesor

--- routes/profesores.js
+++ routes/profesores.js
@@ -7,34 +7,12 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const { check } = require('express-validator');
 const { getProfessors, getProfessorById, addProfessor, updateProfessor, deleteProfessor } = require('../models/profesoresModel');
 
-const { escapeRegExp } = require('escape-string-regexp');
-
-// Ruta para buscar registros con expresiones regulares
-router.get('/buscar', (req, res) => {
-    const searchTerm = req.params.busqueda; // Término de búsqueda
-    const tabla = req.params.tabla;
-
-    // Escapa caracteres especiales en el término de búsqueda
-    const escapedTerm = escapeRegExp(escapedTerm);
-
-    const sql = `SELECT * FROM '${tabla}' WHERE nombre REGEXP '${escapedTerm}'`;
-
-    connection.query(sql, (error, results) => {
-        if (error) {
-            console.error('Error en la consulta', error);
-            res.status(500).json({ error: 'Error en la consulta' });
-        } else {
-            console.log('Resultados: ', results);
-            // Devuelve los resultados como respuesta JSON
-            res.json(results);
-        }
-    });
-});
-
 router.post('/',
     [
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('nombre', 'El nombre debe ser una cadena de caracteres').isString(),
         check('especialidad', 'La especialidad es obligatoria').not().isEmpty(),
+        check('especialidad', 'La especialidad debe ser una cadena de caracteres').isString(),
         check('email', 'El email es inválido').isEmail(),
         validarCampos
     ],
@@ -46,6 +24,6 @@ router.get('/', profesoresController.getProfessors); // Obtener todos los profes
 router.get('/:id', profesoresController.getProfessorById); // Obtener un profesor por su ID
 router.post('/', profesoresController.addProfessor); // Crear un profesor
 router.put('/:id', profesoresController.updateProfessor); // Actualizar un profesor existente
-router.delete('/:id', profesoresController.deleteProfessor); // Remover un profesor
+router.delete('/:id', profesoresController.deleteProfessor); // Quitar un profesor
 
 module.exports = router;
