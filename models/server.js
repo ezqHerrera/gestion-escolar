1ef6862a8fa4cb366669db009499d0734418e499
r0drigoHerrera <rodherrera14@gmail.com>
2023-07-04 20:36:49 -0300

    validaciones estudiantes

--- models/server.js
+++ models/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-// TODO implementar validaciones
 
 class Server {
     constructor(){
--- routes/estudiantes.js
+++ routes/estudiantes.js
@@ -7,37 +7,14 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const { check } = require('express-validator');
 const { getStudents, getStudentById, addStudent, updateStudent, deleteStudent, getStudentsCourses } = require('../models/estudiantesModel');
 
-const { escapeRegExp } = require('escape-string-regexp');
-
-// Ruta para buscar registros con expresiones regulares
-router.get('/buscar', (req, res) => {
-    const searchTerm = req.params.busqueda; // Término de búsqueda
-    const tabla = req.params.tabla;
-
-    // Escapa caracteres especiales en el término de búsqueda
-    const escapedTerm = escapeRegExp(searchTerm);
-
-    const sql = `SELECT * FROM '${tabla}' WHERE nombre REGEXP '${escapedTerm}'`;
-
-    connection.query(sql, (error, results) => {
-        if (error) {
-            console.error('Error en la consulta', error);
-            res.status(500).json({ error: 'Error en la consulta' });
-        } else {
-            console.log('Resultados: ', results);
-            // Devuelve los resultados como respuesta JSON
-            res.json(results);
-        }
-    });
-});
-
 router.post('/',
     [
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('nombre', 'El nombre solo debe contener letras').isString(),
         check('edad', 'La edad debe ser obligatoria').not().isEmpty(),
-        check('edad', 'La edad debe ser numérica').isNumeric,
-        check('grado', 'El grado es obligatorio').not().isEmpty,
-        check('grado', 'El grado debe ser una cadena de caracteres').isString,
+        check('edad', 'La edad debe ser numérica').isNumeric(),
+        check('grado', 'El grado es obligatorio').not().isEmpty(),
+        check('grado', 'El grado debe ser una cadena de caracteres').isString(),
         validarCampos
     ],
     addStudent
